c055e3a63ecab2ba43a8f0fd21da8bc1b920ecc9
akvince <deflandre.vincent@live.fr>
2016-04-12 09:21:23 +0200

    add file of config in config.rb

--- config.rb
+++ config.rb
@@ -1,11 +1,14 @@
-require 'bootstrap-sass'
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
-
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = '/'
+css_dir = 'stylesheets'
+sass_dir = 'sass'
+images_dir = 'img'
+javascripts_dir = 'javascripts'
 
+require 'bootstrap-sass'
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
@@ -14,7 +17,6 @@ http_path = "/"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
